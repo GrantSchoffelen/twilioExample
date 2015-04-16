8dc7c5072acd6504c2d73f2da388fc53dcf74129
GrantSchoffelen <grant.schoffelen@gmail.com>
2015-04-16 15:43:22 -0400

    swag

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var cors = require('cors')
+
+var client = require('twilio')("AC055e2c406321688db01756618570376e", "3ed6d61c3e9a141a97903453820f65ba");
+
+/* GET home page. */
+router.post('/', function(req, res) {
+	console.log(req.body)
+	
+  client.messages.create({
+    body: req.body.title,
+    to: "+16094396656",
+    from: "+16096143170"
+}, function(err, message) {
+    console.log(err)
+});
+
+});
+
+module.exports = router;
