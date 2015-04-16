cd5c44678a69c61634db96d153fbc1220e4c0303
GrantSchoffelen <grant.schoffelen@gmail.com>
2015-04-16 16:14:26 -0400

    swag

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,13 @@ var cors = require('cors')
 
 var client = require('twilio')("AC055e2c406321688db01756618570376e", "3ed6d61c3e9a141a97903453820f65ba");
 
+
 /* GET home page. */
 router.post('/', function(req, res) {
-	console.log(req.body)
+
 	
   client.messages.create({
-    body: req.body.title,
+    body: "Hey Did you remember to "+ req.body.title + "?",
     to: "+16094396656",
     from: "+16096143170"
 }, function(err, message) {
--- routes/response.js
+++ routes/response.js
@@ -5,7 +5,7 @@ var cors = require('cors')
 var client = require('twilio')("AC055e2c406321688db01756618570376e", "3ed6d61c3e9a141a97903453820f65ba");
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.post('/', function(req, res) {
 	console.log('hit response page')
 	console.log(req.body)
 	
