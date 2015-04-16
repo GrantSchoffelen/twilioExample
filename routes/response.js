e05f090f629018cadd45f64aacd178a7216c42fc
GrantSchoffelen <grant.schoffelen@gmail.com>
2015-04-16 17:11:53 -0400

    swag

--- routes/response.js
+++ routes/response.js
@@ -7,9 +7,10 @@ var client = require('twilio')("AC055e2c406321688db01756618570376e", "3ed6d61c3e
 /* GET home page. */
 router.post('/', function(req, res) {
 	console.log('hit response page')
-	console.log(req.body.Body)
+	console.log(req.body, "bodyyyyyyyyyyyyyyyyyyyyyyy")
 
 	if(req.body.Body.toLowerCase() === 'yes'){
+        console.log('hit yes')
 	
   client.messages.create({
     body: "Great job, keep up the good work!",
@@ -21,6 +22,7 @@ router.post('/', function(req, res) {
 }
 
 else if(req.body.Body.toLowerCase() === 'no'){
+    console.log('hit no')
 	
   client.messages.create({
     body: "I hope your feeling alright, would you like me to put you in contact with a super nurse? Enter Yes or No",
